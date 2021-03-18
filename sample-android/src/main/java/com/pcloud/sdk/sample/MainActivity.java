95ce42aa7bd0523b0d90776db00f90b6f9f16fd0
Julian Raufelder <Julian@Raufelder.com>
2021-03-18 17:11:07 +0100

    Test foo

--- sample-android/src/main/java/com/pcloud/sdk/sample/MainActivity.java
+++ sample-android/src/main/java/com/pcloud/sdk/sample/MainActivity.java
@@ -34,68 +34,65 @@ import com.pcloud.sdk.AuthorizationResult;
 import com.pcloud.sdk.DataSource;
 import com.pcloud.sdk.PCloudSdk;
 import com.pcloud.sdk.RemoteFolder;
+import com.pcloud.sdk.internal.IOUtils;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Date;
-import java.util.UUID;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.io.Reader;
 
 public class MainActivity extends Activity {
 
-    private static final int PCLOUD_AUTHORIZATION_REQUEST_CODE = 123;
+	private static final int PCLOUD_AUTHORIZATION_REQUEST_CODE = 123;
 
-    private TextView apiKeyView;
-    private TextView authorizationResultView;
+	private TextView apiKeyView;
+	private TextView authorizationResultView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        authorizationResultView = findViewById(R.id.authorizationResult);
-        apiKeyView = findViewById(R.id.apiKey);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		authorizationResultView = findViewById(R.id.authorizationResult);
+		apiKeyView = findViewById(R.id.apiKey);
 
-        findViewById(R.id.authorizeButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                apiKeyView.setText(null);
-                authorizationResultView.setText(null);
-                //TODO Set YOUR application Client ID
-                Intent authIntent = AuthorizationActivity.createIntent(
-                        MainActivity.this,
-                        AuthorizationRequest.create()
-                                .setType(AuthorizationRequest.Type.TOKEN)
-                                .setClientId("XTsnFgtbuxH")
-                                .setForceAccessApproval(true)
-                                .addPermission("manageshares")
-                                .build());
-                startActivityForResult(authIntent, PCLOUD_AUTHORIZATION_REQUEST_CODE);
-            }
-        });
-    }
+		findViewById(R.id.authorizeButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				apiKeyView.setText(null);
+				authorizationResultView.setText(null);
+				//TODO Set YOUR application Client ID
+				Intent authIntent = AuthorizationActivity.createIntent(MainActivity.this, AuthorizationRequest.create().setType(AuthorizationRequest.Type.TOKEN).setClientId("XTsnFgtbuxH").setForceAccessApproval(true).addPermission("manageshares").build());
+				startActivityForResult(authIntent, PCLOUD_AUTHORIZATION_REQUEST_CODE);
+			}
+		});
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PCLOUD_AUTHORIZATION_REQUEST_CODE) {
-            final AuthorizationData authData = AuthorizationActivity.getResult(data);
-            AuthorizationResult result = authData.result;
-            authorizationResultView.setText(result.name());
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == PCLOUD_AUTHORIZATION_REQUEST_CODE) {
+			final AuthorizationData authData = AuthorizationActivity.getResult(data);
+			AuthorizationResult result = authData.result;
+			authorizationResultView.setText(result.name());
 
-            switch (result) {
-                case ACCESS_GRANTED:
-                    apiKeyView.setText(authData.toString());
+			switch (result) {
+				case ACCESS_GRANTED:
+					apiKeyView.setText(authData.toString());
 
 					Thread thread = new Thread(new Runnable() {
 						@Override
 						public void run() {
-							try  {
+							try {
 								try {
-									ApiClient apiClient = PCloudSdk.newClientBuilder()
-											.authenticator(Authenticators.newOAuthAuthenticator(authData.token))
-											.create();
+									ApiClient apiClient = PCloudSdk.newClientBuilder().apiHost(authData.apiHost).authenticator(Authenticators.newOAuthAuthenticator(authData.token)).create();
 
-									byte[] fileContents = "fooooooooo".getBytes();
+									byte[] ret = "7dkWZDEmrKFWWFwf6nvJX7sHZMqwhHcWydDUgs".getBytes();
 
-									apiClient.createFile(RemoteFolder.ROOT_FOLDER_ID, "foopo.txt", DataSource.create(fileContents)).execute();
+									apiClient.createFile(RemoteFolder.ROOT_FOLDER_ID, "LI65pACAWjvkS-xLXXuAZ49MUMSC6BxBnsc=.c9r", DataSource.create(ret)).execute();
+
 								} catch (IOException e) {
 									e.printStackTrace();
 								} catch (ApiError apiError) {
@@ -110,20 +107,20 @@ public class MainActivity extends Activity {
 					thread.start();
 
 					break;
-                case ACCESS_DENIED:
-                    //TODO: Add proper handling for denied grants.
-                    Log.d("pCloud", "Account access denied");
-                    break;
-                case AUTH_ERROR:
-                    //TODO: Add error handling.
-                    apiKeyView.setText(authData.errorMessage);
-                    Log.d("pCloud", "Account access grant error:\n" + authData.errorMessage);
-                    break;
-                case CANCELLED:
-                    //TODO: Handle cancellation.
-                    Log.d("pCloud", "Account access grant cancelled:");
-                    break;
-            }
-        }
-    }
+				case ACCESS_DENIED:
+					//TODO: Add proper handling for denied grants.
+					Log.d("pCloud", "Account access denied");
+					break;
+				case AUTH_ERROR:
+					//TODO: Add error handling.
+					apiKeyView.setText(authData.errorMessage);
+					Log.d("pCloud", "Account access grant error:\n" + authData.errorMessage);
+					break;
+				case CANCELLED:
+					//TODO: Handle cancellation.
+					Log.d("pCloud", "Account access grant cancelled:");
+					break;
+			}
+		}
+	}
 }
